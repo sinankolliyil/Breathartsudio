88778861ad7f7f27d21a79ac813c8a4ca134deb1
sinan <sinan@breathart.com>
2026-06-04 05:26:26 -0700

    icon changed try again

--- breathart-next/app/layout.js
+++ breathart-next/app/layout.js
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/assets/logo/photography-logo.webp" />
         {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
