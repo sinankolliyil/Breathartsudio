c8b7fb990a3957b23eb514ac33718fff98a7fc95
sinan <sinankolliyil@gmail.com>
2026-07-09 18:16:26 +0530

    google tag replaced

--- breathart-next/app/landing/page.js
+++ breathart-next/app/landing/page.js
@@ -271,11 +271,11 @@ export default function LandingPage() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WRP9H2N5');
+          })(window,document,'script','dataLayer','GTM-THL5B8RG');
         `}
       </Script>
       <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WRP9H2N5"
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THL5B8RG"
         height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
       </noscript>
       <AbstractBackgroundLines />
